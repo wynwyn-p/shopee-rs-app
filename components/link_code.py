fb6b337aab2cc286efc71fb9eba4a390651bf5d9
wynwyn-p <thientrach666@gmail.com>
2025-04-23 22:32:22 +0700

    Update link_code.py

--- components/link_code.py
+++ components/link_code.py
@@ -1,13 +1,10 @@
 import streamlit as st
 
 def show():
-    st.title("LIÊN KẾT MÃ NGUỒN")
+    st.title("📦 LIÊN KẾT MÃ NGUỒN")
 
-    st.markdown("""
-    <div style='font-size: 18px; line-height: 1.8; text-align: justify; background-color: #111111; padding: 20px; border-radius: 10px; color: white;'>
-        📂 <b>Xem toàn bộ mã nguồn của đồ án tại:</b><br>
-        👉 <a href="https://drive.google.com/drive/u/0/folders/1LWkvBy4MUGA08mJMVxFwQs8gKP6VbO-a" target="_blank" style="color:#1faee9; font-weight:bold;">
-            Google Drive - Source Code (Click vào đây)
-        </a>
-    </div>
-    """, unsafe_allow_html=True)
+    st.markdown("### 📁 Xem toàn bộ mã nguồn tại:")
+    
+    st.markdown("🔗 [Google Drive - Source Code](https://drive.google.com/drive/u/0/folders/1LWkvBy4MUGA08mJMVxFwQs8gKP6VbO-a)")
+    
+    st.markdown("🐙 [GitHub Repository](https://github.com/wynwyn-p/shopee-rs-app)")
