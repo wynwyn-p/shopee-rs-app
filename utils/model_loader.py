f56442e2344962a60acabab865d442b83d6241b5
wynwyn-p <thientrach666@gmail.com>
2025-04-23 09:54:20 +0700

    Update model_loader.py

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -8,15 +8,14 @@ from utils.download_models import download_models
 download_models()  # Tự động tải nếu thiếu
 
 @st.cache_resource(show_spinner="Đang tải mô hình...")
-def load_all_models(base_path="models"):
-    df_final = pd.read_parquet(os.path.join(base_path, 'df_final_new.parquet'))
+def load_all_models():
+    base_path = os.path.join(os.path.dirname(__file__), "..", "models")  # tự động lấy đường dẫn
 
+    df_final = pd.read_parquet(os.path.join(base_path, 'df_final_new.parquet'))
     dictionary = corpora.Dictionary.load(os.path.join(base_path, 'dictionary_tokenized.dict'))
     tfidf_model = models.TfidfModel.load(os.path.join(base_path, 'tfidf_model_gensim.pkl'))
 
-    similarity_index_path = os.path.join(base_path, 'gensim_index_merged')
-    similarity_index = similarities.Similarity.load(similarity_index_path)
-
+    similarity_index = similarities.Similarity.load(os.path.join(base_path, 'gensim_index_merged'))
     baseline_model = joblib.load(os.path.join(base_path, 'baseline_only_model.pkl'))
 
     return dictionary, tfidf_model, similarity_index, df_final, baseline_model
